98cd434e1d84a414e6c9179462d3b0d2b48fe9f0
RodRic99 <128597511+RodRic99@users.noreply.github.com>
2025-06-19 01:58:57 +0900

    셀프수정 이제 진짜 시작

--- eternareturn_DB/Get_User_data_py.py
+++ eternareturn_DB/Get_User_data_py.py
@@ -8,52 +8,50 @@ from collections import defaultdict
 class send_erdata():
 
     def __init__(self):
-        test_id="43543072"
-        file_path=r"C:\Users\wnsgu\OneDrive\바탕 화면\eternareturn_DB\api_key\api_key.txt"
-        api_link=r"https://open-api.bser.io/v1/games/"+test_id
-        print(api_link)
+        self.test_id="43543072"
+        self.file_path=r"C:\Users\RodRic\Documents\GitHub\ER_search_character\eternareturn_DB\api_key\api_key.txt"
+        self.api_link=r"https://open-api.bser.io/v1/games/"
+        self.teams=None
+        self.rawdata=None
+        print(self.api_link)
 
 
     def read_api_key(self):
         try:
-            with open(file_path, 'r', encoding='utf-8') as file:
+            with open(self.file_path, 'r', encoding='utf-8') as file:
                 key = file.read()
                 print(key)
         except FileNotFoundError:
-            return print(f"파일을 찾을 수 없습니다: {file_path}")
+            return print(f"파일을 찾을 수 없습니다: {self.file_path}")
         except Exception as e:
              return print(f"파일 읽기 중 오류 발생: {e}")
         headers = {
             "x-api-key": key
         }
         try:
-            response=requests.get(api_link,headers=headers)
+            response=requests.get(self.api_link,headers=headers)
             response.raise_for_status()  # HTTP 오류 발생 시 예외 처리
-            rawdata = response.json()
-            print(rawdata)
+            self.rawdata = response.json()
+            print(self.rawdata)
         except requests.RequestException as e:
             print("요청 중 오류 발생:", e)
-        teams  = defaultdict(list)
-
-
-
-    for game in rawdata.get("userGames", []):
-        team = game.get("gameRank")
-        teams[team].append({
-            "gameid":game.get("gameId")
-            "nickname": game.get("nickname",None),
-            "ranktier": game.get("mmrBefore",None),
-            "getRP": game.get("mmrGain",None),
-            "characterNum": game.get("characterNum", None)
-        })
-    # 각 팀별로 개별 플레이어 통계 출력
-
-    for team_rank, players in teams.items():
-        print(f"팀 (게임 랭크 = {team_rank}):")
-        for player in enumerate(players):
-            print(f"    캐릭터: {player['characterNum']}")
-            print(f"    획득RP: {player['getRP']}")
-        print()
+        self.teams  = defaultdict(list)
+        for game in self.rawdata.get("userGames", []):
+            team = game.get("gameRank")
+            self.teams[team].append({
+                "gameId":       game.get("gameId"),
+                "nickname":     game.get("nickname", None),
+                "rankTier":     game.get("mmrBefore", None),
+                "getRP":        game.get("mmrGain", None),
+                "characterNum": game.get("characterNum", None),
+            })
+        # 각 팀별로 개별 플레이어 통계 출력
+        for team_rank, players in self.teams.items():
+            print(f"팀 (게임 랭크 = {team_rank}):")
+            for player in enumerate(players):
+                print(f"    캐릭터: {player['characterNum']}")
+                print(f"    획득RP: {player['getRP']}")
+            print()
 
     def send_db(self):
         path="./db/db.db"
